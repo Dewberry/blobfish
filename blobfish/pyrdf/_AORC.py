aa06bbd4101d0903bf502ec499678941029d3124
Nick Roberts <njroberts@dewberry.com>
2023-02-23 19:47:21 +0000

    Fix capitalization inconsistency

--- blobfish/pyrdf/_AORC.py
+++ blobfish/pyrdf/_AORC.py
@@ -1,47 +1,46 @@
-        
-from rdflib import URIRef, Namespace         
-from rdflib.namespace import DefinedNamespace         
+from rdflib import URIRef, Namespace
+from rdflib.namespace import DefinedNamespace
 
-        
-class AORC(DefinedNamespace):         
-	"""         
-	AORC Ontology for S3 cloud mirror         
-	Created on 2023-01-21 16:04         
-	"""
-         
-	#Classes
-	CodeRepository: URIRef
-	CommitHash: URIRef
-	CompositeGrid: URIRef
-	CompositeGridURI: URIRef
-	MirrorURI: URIRef
-	RFC: URIRef
-	Script: URIRef
-	SourceDataset: URIRef
-	SourceGrid: URIRef
-	SourceURI: URIRef
 
-	#Data Properties
-	hasDateCreated: URIRef
-	hasRFCAlias: URIRef
-	hasRFCName: URIRef
-	hasRefDate: URIRef
+class AORC(DefinedNamespace):
+    """
+    AORC Ontology for S3 cloud mirror
+    Created on 2023-01-21 16:04
+    """
 
-	#Object Properties
-	IsMirrorURIOf: URIRef
-	hasCodeRepositroy: URIRef
-	hasCommitHash: URIRef
-	hasCompositeGridURI: URIRef
-	hasCreationScript: URIRef
-	hasMirrorURI: URIRef
-	hasRFC: URIRef
-	hasSourceGrid: URIRef
-	hasSourceURI: URIRef
-	isCodeRepositoryOf: URIRef
-	isCommitHashOf: URIRef
-	isCompositeGridURIOf: URIRef
-	isRFCOf: URIRef
-	isSourceGridOf: URIRef
-	isSourceURIOf: URIRef
+    # Classes
+    CodeRepository: URIRef
+    CommitHash: URIRef
+    CompositeGrid: URIRef
+    CompositeGridURI: URIRef
+    MirrorURI: URIRef
+    RFC: URIRef
+    Script: URIRef
+    SourceDataset: URIRef
+    SourceGrid: URIRef
+    SourceURI: URIRef
 
-	_NS = Namespace("http://github.com/Dewberry/blobfish/semantics/rdf/aorc#")
+    # Data Properties
+    hasDateCreated: URIRef
+    hasRFCAlias: URIRef
+    hasRFCName: URIRef
+    hasRefDate: URIRef
+
+    # Object Properties
+    isMirrorURIOf: URIRef
+    hasCodeRepositroy: URIRef
+    hasCommitHash: URIRef
+    hasCompositeGridURI: URIRef
+    hasCreationScript: URIRef
+    hasMirrorURI: URIRef
+    hasRFC: URIRef
+    hasSourceGrid: URIRef
+    hasSourceURI: URIRef
+    isCodeRepositoryOf: URIRef
+    isCommitHashOf: URIRef
+    isCompositeGridURIOf: URIRef
+    isRFCOf: URIRef
+    isSourceGridOf: URIRef
+    isSourceURIOf: URIRef
+
+    _NS = Namespace("http://github.com/Dewberry/blobfish/semantics/rdf/aorc#")
