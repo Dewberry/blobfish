5fcc13917533a3c71288bb9c88131215b542aac7
Nick Roberts <njroberts@dewberry.com>
2023-02-23 16:27:27 +0000

    Added limit and util functions for development

--- blobfish/aorc/transfer.py
+++ blobfish/aorc/transfer.py
@@ -14,7 +14,7 @@ from typing import List
 from requests import Session
 from boto3.resources.factory import ServiceResource
 from dateutil.relativedelta import relativedelta
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 
 from .const import RFC_INFO_LIST, RFCInfo, FIRST_RECORD, FTP_HOST
 from ..utils.gitinfo import GitInfo
@@ -24,6 +24,17 @@ from ..utils.gitinfo import GitInfo
 class SourceURLObject:
     url: str
     date: datetime.datetime
+
+
+@dataclass
+class TransferMetadata:
+    source_uri: str
+    mirror_uri: str
+    ref_date: str
+    mirror_repository: str
+    mirror_commit_hash: str
+    mirror_script: str
+    mirror_active_branch: str
 
 
 class FTPError(Exception):
@@ -62,6 +73,7 @@ class TransferHandler:
         end_date: datetime.datetime = datetime.datetime.today(),
         dev: bool = False,
         concurrency: int = 5,
+        limit: int | None = None,
     ) -> None:
         # Settings for development vs production
         self.dev = dev
@@ -79,6 +91,7 @@ class TransferHandler:
         self.semaphore_size = concurrency
         self.gitinfo = gitinfo
         self.script_path = script_path
+        self.limit = limit
 
     def __create_session(self) -> Session:
         session = requests.Session()
@@ -101,10 +114,10 @@ class TransferHandler:
             with self.requests_session.head(directory_formatted_url) as resp:
                 if resp.status_code != 200:
                     raise FTPError
+        logging.info("expected file structure of FTP server verified")
 
     def __create_url_list(self) -> List[SourceURLObject]:
         urls = []
-
         file_template_url = "AORC_{0}RFC_4km/{0}RFC_precip_partition/AORC_APCP_4KM_{0}RFC_{1}.zip"
         try:
             self.__verify()
@@ -116,8 +129,12 @@ class TransferHandler:
                     )
                     urls.append(SourceURLObject(file_formatted_url, current_datetime))
                     current_datetime += relativedelta(months=1)
+                    if self.limit and len(urls) >= self.limit:
+                        break
+                if self.limit and len(urls) >= self.limit:
+                    break
         except FTPError:
-            logging.error("expected file structure is not verified")
+            logging.error("expected file structure of FTP server was not verified")
             sys.exit(1)
         return urls
 
@@ -139,18 +156,23 @@ class TransferHandler:
             await outfile.write(data)
 
     async def __transfer_data(self, url_object: SourceURLObject, sem: asyncio.BoundedSemaphore, session: ClientSession):
-        mirror_uri = f"{self.mirror_file_prefix}/{url_object.url.replace(FTP_HOST, '')}"
-        upload_meta = {
-            "SourceURI": url_object.url,
-            "MirrorURI": mirror_uri,
-            "RefDate": url_object.date.strftime("%Y-%m-%d"),
-        }
+        mirror_uri = f"s3://{self.mirror_bucket_name}/{self.mirror_file_prefix}/{url_object.url.replace(FTP_HOST, '')}"
+        upload_meta = TransferMetadata(
+            url_object.url,
+            mirror_uri,
+            url_object.date.strftime("%Y-%m-%d"),
+            self.gitinfo.origin_url,
+            self.gitinfo.commit_hash,
+            self.script_path,
+            self.gitinfo.active_branch,
+        )
         mirror_bucket = self.resource.Bucket(self.mirror_bucket_name)
         with tempfile.TemporaryFile() as fp:
             data = await self.__get_data(url_object.url, sem, session)
             if data:
                 await self.__write_data(data, fp)
-                mirror_bucket.upload_fileobj(fp, mirror_uri, ExtraArgs=upload_meta)
+                mirror_bucket.upload_fileobj(fp, mirror_uri, ExtraArgs={"Metadata": asdict(upload_meta)})
+                logging.info(f"data from {url_object.url} successfully transferred to {mirror_uri}")
             else:
                 logging.error(f"tried to transfer data for {url_object.url}, received no data")
 
@@ -158,24 +180,57 @@ class TransferHandler:
         url_objects = self.__create_url_list()
         tasks = []
         sem = asyncio.BoundedSemaphore(self.semaphore_size)
-        session = ClientSession()
-        for url_object in url_objects:
-            task = asyncio.create_task(self.__transfer_data(url_object, sem, session))
-            tasks.append(task)
-        download_paths = await asyncio.gather(*tasks)
-        await session.close()
-        return download_paths
+        async with ClientSession() as session:
+            for url_object in url_objects:
+                task = asyncio.create_task(self.__transfer_data(url_object, sem, session))
+                tasks.append(task)
+                logging.info(f"async task created to mirror data from {url_object.url}")
+            download_paths = await asyncio.gather(*tasks)
+            return download_paths
 
     def transfer_files(self) -> List[str]:
+        logging.info("starting async task gathering")
         download_paths = asyncio.run(self.__gather_download_tasks())
         return download_paths
+
+
+def clear_downloads(bucket: str, prefix: str):
+    client = boto3.client(
+        service_name="s3",
+        aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+        region_name=os.environ["AWS_DEFAULT_REGION"],
+    )
+    paginator = client.get_paginator("list_objects_v2")
+    for page in paginator.paginate(Bucket=bucket, Prefix=prefix):
+        contents = page.get("Contents", [])
+        for content in contents:
+            client.delete_object(Bucket=bucket, Key=content.get("Key"))
+
+
+def view_downloads(bucket: str, prefix: str):
+    client = boto3.client(
+        service_name="s3",
+        aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+        region_name=os.environ["AWS_DEFAULT_REGION"],
+    )
+    paginator = client.get_paginator("list_objects_v2")
+    for page in paginator.paginate(Bucket=bucket, Prefix=prefix):
+        contents = page.get("Contents", [])
+        for content in contents:
+            print(client.get_object(Bucket=bucket, Key=content.get("Key")))
 
 
 if __name__ == "__main__":
     from ..utils.gitinfo import version, script
 
     load_dotenv()
-    git_info = version()
-    script_path = script(__file__)
-    transfer_handler = TransferHandler(git_info, script_path, "tempest", "test", dev=True)
-    transfer_handler.transfer_files()
+
+    # git_info = version()
+    # script_path = script(__file__)
+    # transfer_handler = TransferHandler(git_info, script_path, "tempest", "test", dev=True)
+    # transfer_handler.transfer_files()
+
+    # clear_downloads("tempest", "test/AORC")
+    view_downloads("tempest", "test/AORC")
