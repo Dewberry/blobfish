329669a3c5032e3980fa397c21d519c3b3fcfd25
Nick Roberts <njroberts@dewberry.com>
2023-03-27 19:41:58 +0000

    Reuse cloud client

--- blobfish/aorc/update_metadata.py
+++ blobfish/aorc/update_metadata.py
@@ -1,15 +1,18 @@
-from ..utils.cloud_utils import get_s3_content, update_metadata
-from .const import FTP_HOST, RFC_INFO_LIST, RFCInfo
+from ..utils.cloud_utils import get_s3_content, update_metadata, check_exists, get_client
+from .const import FTP_HOST, RFC_INFO_LIST
 from .transfer import TransferMetadata
+from .composite import CompositeMembershipMetadata
 from dataclasses import asdict
-from typing import cast
+from typing import cast, Any
 import datetime
 import os
+import logging
 
 
-class MetaBuilder:
-    def __init__(self, s3_object: dict):
+class TransferMetaBuilder:
+    def __init__(self, s3_object: dict, client: Any | None):
         self.base = s3_object
+        self.client = client
         self.__verify()
         self.bucket = cast(str, self.base.get("Bucket"))
         self.mirror_uri = cast(str, self.base.get("Key"))
@@ -25,9 +28,17 @@ class MetaBuilder:
         self.source_bytes = self.__identify_bytes()
 
     def __verify(self):
-        metadata = cast(str, self.base.get("Metadata"))
+        metadata = cast(dict, self.base.get("Metadata"))
         if len(metadata) > 0:
-            raise ValueError(f"This object has unexpected existing metadata: {metadata}")
+            try:
+                TransferMetadata(**metadata)
+            except TypeError:
+                logging.info(f"Metadata for {self.base.get('Key')} is incomplete, continuing to creation")
+            else:
+                logging.info(f"This object has metadata which seems to have already been updated")
+                raise ValueError
+        else:
+            logging.info(f"Metadata for {self.base.get('Key')} empty, continuing to creation")
 
     def __identify_rfc_info(self) -> tuple[str, str]:
         end_pos = self.mirror_uri.find("RFC")
@@ -36,7 +47,8 @@ class MetaBuilder:
         for rfc in RFC_INFO_LIST:
             if rfc.alias == alias:
                 return rfc.alias, rfc.name
-        raise AttributeError(f"No matching rfc found for {self.mirror_uri}")
+        logging.error(f"No matching rfc found for {self.mirror_uri}")
+        raise AttributeError()
 
     def __construct_catalog_url(self) -> str:
         return f"/AORC_{self.rfc_alias}FC_4km"
@@ -51,7 +63,6 @@ class MetaBuilder:
         return date_dt.strftime("%Y-%m-%d")
 
     def __construct_url(self) -> str:
-        # https://hydrology.nws.noaa.gov/pub/aorc-historic/AORC_ABRFC_4km/ABRFC_precip_partition/AORC_APCP_4KM_ABRFC_197902.zip
         fn = self.mirror_uri.split("/")[-1]
         formatted = f"{FTP_HOST}/AORC_{self.rfc_alias}RFC_4km/{self.rfc_alias}RFC_precip_partition/{fn}"
         return formatted
@@ -77,37 +88,107 @@ class MetaBuilder:
         )
 
 
-def retrieve_mirrors(bucket: str, prefix: str):
-    for obj in get_s3_content(bucket, prefix, True):
+class CompositeMetaBuilder:
+    def __init__(self, s3_object: dict, client: Any | None):
+        self.base = s3_object
+        self.client = client
+        self.__verify()
+        self.bucket = cast(str, self.base.get("Bucket"))
+        self.key = cast(str, self.base.get("Key"))
+        self.zarr_key = self.__identify_zarr()
+        self.start_time_dt = self.__identify_temporal_coverage()
+        self.end_time_dt = self.start_time_dt + datetime.timedelta(hours=1)
+        self.members = self.__identify_members()
+        self.docker_image_url = f"https://hub.docker.com/layers/njroberts/blobfish-python/{os.environ['TAG']}/images/{os.environ['HASH']}?context=repo"
+        self.composite_script = "proj_blobfish_aorc_composite.py"
+
+    def __verify(self):
+        metadata = cast(dict, self.base.get("Metadata"))
+        if len(metadata) > 0:
+            try:
+                self.from_dict(metadata)
+            except KeyError:
+                logging.info(f"Metadata for {self.base.get('Key')} is incomplete, continuing to creation")
+            else:
+                logging.info(f"This object has metadata which seems to have already been updated")
+                raise ValueError
+        else:
+            logging.info(f"Metadata for {self.base.get('Key')} empty, continuing to creation")
+
+    def __identify_zarr(self) -> str:
+        zarr_end_pos = self.key.find(".zarr") + len(".zarr")
+        return self.key[:zarr_end_pos]
+
+    def __identify_temporal_coverage(self) -> datetime.datetime:
+        fn = self.zarr_key.split("/")[-1].replace(".zarr", "")
+        dt = datetime.datetime.strptime(fn, "%Y%m%d%H")
+        return dt
+
+    def __identify_members(self) -> set[str]:
+        member_set = set()
+        for rfc_info in RFC_INFO_LIST:
+            key = f"mirrors/aorc/precip/AORC_{rfc_info.alias}RFC_4km/{rfc_info.alias}RFC_precip_partition/AORC_APCP_4KM_{rfc_info.alias}RFC_{self.start_time_dt.strftime('%Y%m')}.zip"
+            full_path = f"s3://{self.bucket}/{key}"
+            if check_exists(self.bucket, key, self.client):
+                member_set.add(full_path)
+            else:
+                logging.error(f"Supposed member of {self.zarr_key}, {full_path}, does not exist")
+                raise AttributeError
+        return member_set
+
+    def serialize(self) -> dict:
+        meta = CompositeMembershipMetadata(
+            self.start_time_dt, self.docker_image_url, self.composite_script, self.members
+        )
+        serialized = meta.serialize()
+        return serialized
+
+    @staticmethod
+    def from_dict(metadata_dictionary: dict) -> CompositeMembershipMetadata:
+        start_time = datetime.datetime.fromisoformat(cast(str, metadata_dictionary.get("start_time")))
+        docker_image = cast(str, metadata_dictionary.get("docker_image"))
+        script_name = cast(str, metadata_dictionary.get("composite_script"))
+        members = set(cast(str, metadata_dictionary.get("members")).split(","))
+        return CompositeMembershipMetadata(start_time, docker_image, script_name, members)
+
+
+def retrieve_mirrors(bucket: str, prefix: str, client):
+    for obj in get_s3_content(bucket, prefix, True, client):
         try:
             update_mirror(obj, bucket)
         except ValueError:
-            print(f"Object {obj.get('Key')} metadata has already been updated, skipping")
+            logging.info(f"Object {obj.get('Key')} metadata has already been updated, skipping")
 
 
-def update_mirror(mirror_object: dict, bucket: str) -> None:
-    new_meta_obj = MetaBuilder(mirror_object)
+def update_mirror(mirror_object: dict, bucket: str, client: Any | None = None) -> None:
+    new_meta_obj = TransferMetaBuilder(mirror_object, client)
     transfer_metadata = asdict(new_meta_obj.as_transfer_metadata())
-    update_metadata(bucket=bucket, key=new_meta_obj.mirror_uri, new_meta=transfer_metadata)
+    update_metadata(bucket, new_meta_obj.mirror_uri, transfer_metadata, client)
 
 
-# def retrieve_composites():
-#     pass
+def retrieve_composites(bucket: str, prefix: str, client):
+    for obj in get_s3_content(bucket, prefix, True, client):
+        try:
+            update_composite(obj, bucket)
+        except ValueError:
+            logging.info(f"Object {obj.get('Key')} metadata has already been updated, skipping")
 
 
-# def update_composite():
-#     pass
+def update_composite(mirror_object: dict, bucket: str, client: Any | None = None):
+    new_meta_obj = CompositeMetaBuilder(mirror_object, client)
+    composite_metadata = new_meta_obj.serialize()
+    update_metadata(bucket, new_meta_obj.key, composite_metadata, client)
 
-
-# def main():
-#     for mirror_object in retrieve_mirrors():
-#         update_mirror()
-#     for composite_object in retrieve_composites():
-#         update_composite
 
 if __name__ == "__main__":
     from dotenv import load_dotenv
+    from ..utils.logger import set_up_logger
 
+    set_up_logger(level=logging.INFO)
     load_dotenv()
 
-    retrieve_mirrors("tempest", "aorc/precip/source")
+    bucket = "tempest"
+    client = get_client()
+
+    # retrieve_mirrors(bucket, "mirrors/aorc/precip", client)
+    retrieve_composites(bucket, "transforms/aorc/precipitation", client)
