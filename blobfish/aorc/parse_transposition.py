91905d2fe78b798748e751933b114fdf4f016c69
Seth Lawler <slawler@dewberry.com>
2023-04-17 14:00:20 -0400

    add minor changes to language in kanawha writer

--- blobfish/aorc/parse_transposition.py
+++ blobfish/aorc/parse_transposition.py
@@ -43,7 +43,6 @@ class JSONLDConstructor:
         self.datasets = []
         self.limit = limit
 
-
     def __get_key(self) -> Generator[str, None, None]:
         i = 0
         pattern_string = r".*\.json$"
@@ -64,7 +63,15 @@ class JSONLDConstructor:
     def populate_datasets(self) -> None:
         logging.info(f"Retrieving keys for {self.bucket}")
         for obj_key in self.__get_key():
-            dataset = DatasetConstructor(self.bucket, obj_key, self.script_path, self.docker_path, self.contact_name, self.contact_email, self.client)
+            dataset = DatasetConstructor(
+                self.bucket,
+                obj_key,
+                self.script_path,
+                self.docker_path,
+                self.contact_name,
+                self.contact_email,
+                self.client,
+            )
             ds_json = dataset.create()
             self.datasets.append(ds_json)
 
@@ -87,25 +94,31 @@ class JSONLDConstructor:
         dictionary = json.dumps(self.as_catalog(), indent=4)
         upload_body(self.bucket, key, dictionary, self.client)
 
+
 class DatasetConstructor:
-    def __init__(self, bucket: str, key: str, script_path: str, docker_path: str, contact_name: str, contact_email, client: Any | None) -> None:
+    def __init__(
+        self,
+        bucket: str,
+        key: str,
+        script_path: str,
+        docker_path: str,
+        contact_name: str,
+        contact_email,
+        client: Any | None,
+    ) -> None:
         self.bucket = bucket
         self.key = key
         self.client = client
         self.documentation = self.__get_documentation()
-        self.contact_point = {
-            "@type": "vcard:Contact",
-            "fn": contact_name,
-            "hasEmail": contact_email
-        }
+        self.contact_point = {"@type": "vcard:Contact", "fn": contact_name, "hasEmail": contact_email}
         self.transpositionJob = {
             "@type": "aorc:TranspositionJob",
             "wasStartedBy": {
                 "@type": "aorc:TranspositionScript",
                 "@id": script_path,
                 "dockerImage": docker_path,
-                "description": "The script which is accessible at its IRI path once inside the docker image to which it belongs"
-            }
+                "description": "The script which is accessible at its IRI path once inside the docker image to which it belongs",
+            },
         }
 
     def __get_documentation(self) -> TranspositionDocumentation:
@@ -171,17 +184,14 @@ class DatasetConstructor:
 
     def __find_source_zarrs(self) -> list[dict]:
         # Get assumed s3 key for zarr files used in generation of modeled precip
-        logging.info("Finding AORC precipitation files associated with model")
+        logging.info("Finding AORC precipitation files associated with watershed and transposition domain")
         sources = []
         start_date = self.documentation.format_start_date()
         current_date = start_date
         end_date = start_date + datetime.timedelta(hours=self.documentation.duration)
         while current_date < end_date:
             path = f"s3://tempest/transforms/aorc/precipitation/{current_date.year}/{current_date.strftime('%Y%m%d%H')}.zarr"
-            composite = {
-                "@type": "aorc:CompositeDataset",
-                "@id": path
-            }
+            composite = {"@type": "aorc:CompositeDataset", "@id": path}
             bucket, key = extract_bucketname_and_keyname(path)
             key += "/.zmetadata"
             if check_exists(bucket, key, self.client):
@@ -191,50 +201,54 @@ class DatasetConstructor:
             current_date += datetime.timedelta(hours=1)
         return sources
 
-
     def create(self) -> dict:
         logging.info(f"Creating dataset using documentation from {self.key}")
         dataset = {}
-        dataset["title"] = f"Storm Transposition Model, {self.documentation.metadata.watershed_name} {self.documentation.format_start_date().strftime('%Y-%m-%d')}"
-        dataset["description"] = f"Data documenting modeling of transposition region for {self.documentation.metadata.watershed_name}, {self.documentation.format_start_date().strftime('%Y-%m-%d')}"
+        dataset[
+            "title"
+        ] = f"Storm Transposition Data, {self.documentation.metadata.watershed_name} {self.documentation.format_start_date().strftime('%Y-%m-%d')}"
+        dataset[
+            "description"
+        ] = f"Data documenting development of transposition region for {self.documentation.metadata.watershed_name}, {self.documentation.format_start_date().strftime('%Y-%m-%d')}"
         dataset["keyword"] = [
-                "aorc",
-                "transposition",
-                self.documentation.metadata.watershed_name,
-                "storm",
-                "hydrological",
-                "modeling",
-                "hydrology",
-                "precipitation"
+            "aorc",
+            "transposition",
+            self.documentation.metadata.watershed_name,
+            "storm",
+            "hydrological",
+            "modeling",
+            "hydrology",
+            "precipitation",
         ]
         dataset["modified"] = self.documentation.format_start_date().isoformat()
         dataset["publisher"] = {
+            "@type": "org:Organization",
+            "name": "PTS",
+            "suborganizationOf": {
                 "@type": "org:Organization",
-                "name": "Federal Emergency Management Agency",
-                "suborganizationOf": {
-                    "@type": "org:Organization",
-                    "name": "Department of Homeland Security",
-                    "suborganizationOf": {
-                        "@type": "org:Organization",
-                        "name": "United States Government"
-                    }
-                }
-            }
+                "name": "FEMA",
+                "suborganizationOf": {"@type": "org:Organization", "name": "United States Government"},
+            },
+        }
         dataset["contactPoint"] = self.contact_point
         dataset["identifier"] = "http://dx.doi.org/nonfunctioning/doi/example"
         dataset["accessLevel"] = "public"
-        dataset["bureauCode"] = "024:070",
+        dataset["bureauCode"] = ("024:070",)
         dataset["programCode"] = "024:006"
-        dataset["rights"] = "This dataset is located on an s3 bucket which requires an AWS account which has been granted access to said bucket"
+        dataset[
+            "rights"
+        ] = "This dataset is located on an s3 bucket which requires an AWS account which has been granted access to said bucket"
         dataset["spatial"] = f"{self.documentation.geom.center_y}, {self.documentation.geom.center_x}"
         dataset["temporal"] = f"{self.documentation.format_start_date().isoformat()}/P{self.documentation.duration}"
-        dataset["distribution"] = [{
-                    "@type": "dcat:Distribution",
-                    "description": f"DSS file created by storm transposition model for {self.documentation.metadata.watershed_name}, {self.documentation.format_start_date().strftime('%Y-%m-%d')}",
-                    "downloadURL": f"{create_s3_path(self.bucket, self.__create_dss())}",
-                    "format": "Data Storage System",
-                    "mediaType": "application/octet"
-                }]
+        dataset["distribution"] = [
+            {
+                "@type": "dcat:Distribution",
+                "description": f"DSS file created by storm transposition model for {self.documentation.metadata.watershed_name}, {self.documentation.format_start_date().strftime('%Y-%m-%d')}",
+                "downloadURL": f"{create_s3_path(self.bucket, self.__create_dss())}",
+                "format": "Data Storage System",
+                "mediaType": "application/octet",
+            }
+        ]
         dataset["language"] = ["en"]
         dataset["season"] = self.documentation.start.season
         dataset["waterYear"] = self.documentation.start.water_year
@@ -242,41 +256,42 @@ class DatasetConstructor:
             "@type": "geo:Geometry",
             "description": f"Geometry of the bounding box for {self.documentation.metadata.watershed_name}",
             "region": self.__fetch_wkt(self.documentation.metadata.watershed_source, bbox=True),
-            "name": self.documentation.metadata.watershed_name
+            "name": self.documentation.metadata.watershed_name,
         }
         dataset["transposition"] = {
             "@type": "geo:Geometry",
             "description": f"Geometry of the bounding box which contains the transposition region associated with watershed",
             "region": self.__fetch_wkt(self.documentation.metadata.transposition_domain_source, bbox=True),
-            "name": self.documentation.metadata.transposition_domain_name
+            "name": self.documentation.metadata.transposition_domain_name,
         }
         dataset["image"] = {
-                "@type": "dcat:Dataset",
-                "title": f"Storm Transposition Model, {self.documentation.metadata.watershed_name} {self.documentation.format_start_date().strftime('%Y-%m-%d')}",
-                "modified": f"{self.__fetch_modification(self.__create_png())}",
-                "description": f"Image of watershed transposition results for {self.documentation.metadata.watershed_name} watershed, {self.documentation.format_start_date().strftime('%Y-%m-%d')}",
-                "distribution": [
-                    {
-                        "@type": "dcat:Distribution",
-                        "description": f"PNG file for {self.documentation.metadata.watershed_name} {self.documentation.format_start_date().strftime('%Y-%m-%d')} model",
-                        "downloadURL": f"{create_s3_path(self.bucket, self.__create_png())}",
-                        "format": "PNG Image",
-                        "mediaType": "image/png"
-                    }
-                ]
-            }
+            "@type": "dcat:Dataset",
+            "title": f"Storm Transposition Dataset, {self.documentation.metadata.watershed_name} {self.documentation.format_start_date().strftime('%Y-%m-%d')}",
+            "modified": f"{self.__fetch_modification(self.__create_png())}",
+            "description": f"Image of watershed transposition results for {self.documentation.metadata.watershed_name} watershed, {self.documentation.format_start_date().strftime('%Y-%m-%d')}",
+            "distribution": [
+                {
+                    "@type": "dcat:Distribution",
+                    "description": f"PNG file for {self.documentation.metadata.watershed_name} {self.documentation.format_start_date().strftime('%Y-%m-%d')} dataset",
+                    "downloadURL": f"{create_s3_path(self.bucket, self.__create_png())}",
+                    "format": "PNG Image",
+                    "mediaType": "image/png",
+                }
+            ],
+        }
         dataset["stats"] = {
-                "@type": "aorc:TranspositionStatistics",
-                "count": self.documentation.stats.count,
-                "mean": self.documentation.stats.mean,
-                "max": self.documentation.stats.max,
-                "min": self.documentation.stats.min,
-                "sum": self.documentation.stats.sum,
-                "normMean": self.documentation.stats.norm_mean
-            }
+            "@type": "aorc:TranspositionStatistics",
+            "count": self.documentation.stats.count,
+            "mean": self.documentation.stats.mean,
+            "max": self.documentation.stats.max,
+            "min": self.documentation.stats.min,
+            "sum": self.documentation.stats.sum,
+            "normMean": self.documentation.stats.norm_mean,
+        }
         dataset["wasGeneratedBy"] = self.transpositionJob
         dataset["source"] = self.__find_source_zarrs()
         return dataset
+
 
 def create_s3_path(bucket: str, key: str) -> str:
     return f"s3://{bucket}/{key}"
@@ -292,20 +307,22 @@ if __name__ == "__main__":
 
     set_up_logger(level=logging.INFO)
 
-    client = get_client()
-    year = 2016
-    logging.info(f"Creating JSON-LD catalog for {year}")
-    constructor = JSONLDConstructor(
-        "https://ckan.dewberryanalytics.com/dataset/56bb8b57-e9e7-41bf-a347-c8a467ba1c68/resource/3101b1f8-db5d-43fe-9159-1342be080480/download/sstcatalog.jsonld",
-        "https://ckan.dewberryanalytics.com/dataset/56bb8b57-e9e7-41bf-a347-c8a467ba1c68/resource/0b6b1035-cede-49a9-9dc3-36dcbcb50cbc/download/sstcatalog.json",
-        "tempest",
-        f"watersheds/kanawha/kanawha-transpo-area-v01/72h/docs/{year}",
-        "extract_storms_v2.py",
-        "https://hub.docker.com/layers/placeholder",
-        "Nicholas Roberts",
-        "njroberts@dewberry.com",
-        client,
-        None
-    )
-    constructor.populate_datasets()
-    constructor.serialize_to_s3(f"graphs/aorc/precip/transposition/kanawha-transpo-area-v01/{year}.jsonld")
+    for year in range(1979, 2023):
+
+        client = get_client()
+        # year = 2016
+        logging.info(f"Creating JSON-LD catalog for {year}")
+        constructor = JSONLDConstructor(
+            "https://ckan.dewberryanalytics.com/dataset/56bb8b57-e9e7-41bf-a347-c8a467ba1c68/resource/3101b1f8-db5d-43fe-9159-1342be080480/download/sstcatalog.jsonld",
+            "https://ckan.dewberryanalytics.com/dataset/56bb8b57-e9e7-41bf-a347-c8a467ba1c68/resource/0b6b1035-cede-49a9-9dc3-36dcbcb50cbc/download/sstcatalog.json",
+            "tempest",
+            f"watersheds/kanawha/kanawha-transpo-area-v01/72h/docs/{year}",
+            "extract_storms_v2.py",
+            "172866912423.dkr.ecr.us-east-1.amazonaws.com/stormcloud",
+            "Seth Lawler",
+            "slawler@dewberry.com",
+            client,
+            None,
+        )
+        constructor.populate_datasets()
+        constructor.serialize_to_s3(f"graphs/aorc/precip/transposition/kanawha-transpo-area-v01/{year}.jsonld")
