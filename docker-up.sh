c984c0ba755a98e7d96b1dfbe42f27e5e6dde2d1
Nick Roberts <njroberts@dewberry.com>
2023-06-20 21:16:06 +0000

    Fix reachable git url check

--- docker-up.sh
+++ docker-up.sh
@@ -7,19 +7,27 @@ if [[ $(git status --porcelain) ]]; then
   echo "There are uncommitted changes. Exiting."
   sleep 2 && echo 1
 fi
+
 # Check if current git branch has changes not tracked on repo
 [[ $(git rev-parse HEAD) = $(git rev-parse @{u}) ]] || echo "There are changes not pushed to remote. Exiting." && sleep 2 && exit 1
 if
-# Check that the git hash is a reachable URL
+
+# Get git hash
 git_hash=$(git config --get remote.origin.url)/commit/$(git rev-parse HEAD)
-response=$(curl --write-out %{http_code} --silent --output /dev/null $git_hash)
+
+# Check that git hash is reachable as URL
+git_url=$(git_hash | sed 's/git/https/;s/:/\//' | sed 's/\.git$//')
+response=$(curl --write-out %{http_code} --silent --output /dev/null $git_url)
 if [ $response -ne 200 ]; then
   echo "Error: HTTP status code $response"
   sleep 2 && exit 1
 fi
+
 # Export docker image hash
 export DOCKER_HASH=$(docker inspect --format='{{index .RepoDigests 0}}' $1);
+
 # Export git hash
 export GIT_HASH=$git_hash;
+
 # Use compose file to orchestrate docker container creation
 docker compose up
