74deb3163638857d24c46e6c52b72ab11783f3da
Nick Roberts <njroberts@dewberry.com>
2023-07-02 16:28:42 -0400

    Add env vars

--- docker-up.sh
+++ docker-up.sh
@@ -16,11 +16,12 @@ fi
 # Check if current git branch has changes not tracked on repo
 [[ $(git rev-parse HEAD) = $(git rev-parse @{u}) ]] || echo "There are changes not pushed to remote. Exiting." && sleep 2 && exit 1
 
-# Get git hash
-git_hash=$(git config --get remote.origin.url)/commit/$(git rev-parse HEAD)
+# Get git hash URI
+git_hash_uri=$(git config --get remote.origin.url)/commit/$(git rev-parse HEAD)
 
-# Check that git hash is reachable as URL
-git_url=$(git_hash | sed 's/git/https/;s/:/\//' | sed 's/\.git$//')
+# Check that git hash is reach URIable as URL
+git_url=$(echo $git_hash_uri | sed --expression='s/git@/https:\/\//' | sed --expression='s/\.git//' | sed --expression='s/.com:/.com\//')
+echo $git_url
 response=$(curl --write-out %{http_code} --silent --output /dev/null $git_url)
 if [ $response -ne 200 ]; then
   echo "Error: HTTP status code $response"
@@ -36,8 +37,23 @@ docker_hash=$(docker inspect --format='{{index .RepoDigests 0}}' $1)
 # Export docker image hash
 export DOCKER_HASH=$docker_hash
 
+# Export docker image name with default docker repo of docker.io
+export DOCKER_IMAGE="docker.io/$1"
+
+# Export docker image digest url with default docker repo of docker.io
+export DOCKER_URL="docker.io/$docker_hash"
+
+# Export relative docker file path
+export DOCKER_FILE_PATH=$(find . -name "Dockerfile" -type f -printf "%P")
+
+# Export relative docker compose file path
+export COMPOSE_FILE_PATH=$(find . -name "docker-compose.yml" -type f -printf "%P")
+
 # Export git hash
-export GIT_HASH=$git_hash
+export GIT_HASH=$(git rev-parse HEAD)
+
+# Export git repo with hash url
+export GIT_REPO=$git_url
 
 # Use compose file to orchestrate docker container creation
 docker compose up
