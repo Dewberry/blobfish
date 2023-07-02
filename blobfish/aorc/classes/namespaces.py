8baa31a9786fd36b39441ed4b7c407ff148973a3
Nick Roberts <njroberts@dewberry.com>
2023-07-02 18:56:23 -0400

    Fix namespace for IANA

--- blobfish/aorc/classes/namespaces.py
+++ blobfish/aorc/classes/namespaces.py
@@ -30,6 +30,6 @@ class AORC(DefinedNamespace):
     _NS = Namespace("https://raw.githubusercontent.com/Dewberry/blobfish/v0.9/semantics/rdf/aorc.ttl")
 
 
-IANA_APP = Namespace("https://www.iana.org/assignments/media-types/application/>")
+IANA_APP = Namespace("https://www.iana.org/assignments/media-types/application/")
 EU = Namespace("https://publications.europa.eu/resource/authority/file-type/")
 LOCN = Namespace("http://www.w3.org/ns/locn#")
