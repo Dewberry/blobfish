53d7877c88f2dfb951df7a655dc258a10db4c14d
Nick Roberts <njroberts@dewberry.com>
2023-07-02 19:12:11 -0400

    Comment unused graph creator class

--- blobfish/aorc/classes/common.py
+++ blobfish/aorc/classes/common.py
@@ -15,56 +15,56 @@ class ProvenanceMetadata:
     digest_hash: str
 
 
-class GraphCreator:
-    def __init__(self, shacl_fn: str | None, ont_fn: str | None) -> None:
-        self.tracked_graphs: dict[Any, rdflib.Graph] = {}
-        self.shacl_graph = self.__load_shacl(shacl_fn)
-        self.ont_graph = self.__load_ont(ont_fn)
+# class GraphCreator:
+#     def __init__(self, shacl_fn: str | None, ont_fn: str | None) -> None:
+#         self.tracked_graphs: dict[Any, rdflib.Graph] = {}
+#         self.shacl_graph = self.__load_shacl(shacl_fn)
+#         self.ont_graph = self.__load_ont(ont_fn)
 
-    @staticmethod
-    def __load_shacl(shacl_fn: str | None) -> rdflib.Graph | None:
-        if shacl_fn:
-            graph = rdflib.Graph()
-            graph.parse(shacl_fn)
-            return graph
-        return shacl_fn
+#     @staticmethod
+#     def __load_shacl(shacl_fn: str | None) -> rdflib.Graph | None:
+#         if shacl_fn:
+#             graph = rdflib.Graph()
+#             graph.parse(shacl_fn)
+#             return graph
+#         return shacl_fn
 
-    @staticmethod
-    def __load_ont(ont_fn: str | None) -> rdflib.Graph | None:
-        if ont_fn:
-            graph = rdflib.Graph()
-            graph.parse(ont_fn)
-            return graph
-        return ont_fn
+#     @staticmethod
+#     def __load_ont(ont_fn: str | None) -> rdflib.Graph | None:
+#         if ont_fn:
+#             graph = rdflib.Graph()
+#             graph.parse(ont_fn)
+#             return graph
+#         return ont_fn
 
-    def __create_graph(self, id: str | None = None) -> rdflib.Graph:
-        graph = rdflib.Graph()
-        if id:
-            self.tracked_graphs[id] = graph
-        return graph
+#     def __create_graph(self, id: str | None = None) -> rdflib.Graph:
+#         graph = rdflib.Graph()
+#         if id:
+#             self.tracked_graphs[id] = graph
+#         return graph
 
-    def get_graph(self, id: str | None = None):
-        if id:
-            for graph in self.tracked_graphs:
-                if graph.id == id:
-                    return graph
-            else:
-                return self.__create_graph(id)
-        else:
-            return self.__create_graph()
+#     def get_graph(self, id: str | None = None):
+#         if id:
+#             for graph in self.tracked_graphs:
+#                 if graph.id == id:
+#                     return graph
+#             else:
+#                 return self.__create_graph(id)
+#         else:
+#             return self.__create_graph()
 
-    def validate_graphs(self, **kwargs):
-        if self.shacl_graph:
-            for graph in self.tracked_graphs.values():
-                validate_args = {"data_graph": graph, "shacl_graph": self.shacl_graph}
-                if self.ont_graph:
-                    validate_args["ont_graph"] = self.ont_graph
-                if kwargs:
-                    validate_args.update(kwargs)
-                validate(**validate_args)
-        else:
-            raise ValueError("No shacl graph associated with graph creator, validation failed")
+#     def validate_graphs(self, **kwargs):
+#         if self.shacl_graph:
+#             for graph in self.tracked_graphs.values():
+#                 validate_args = {"data_graph": graph, "shacl_graph": self.shacl_graph}
+#                 if self.ont_graph:
+#                     validate_args["ont_graph"] = self.ont_graph
+#                 if kwargs:
+#                     validate_args.update(kwargs)
+#                 validate(**validate_args)
+#         else:
+#             raise ValueError("No shacl graph associated with graph creator, validation failed")
 
-    def upload_graphs_to_ckan(self):
-        for id, graph in self.tracked_graphs.items():
-            pass
+#     def upload_graphs_to_ckan(self):
+#         for id, graph in self.tracked_graphs.items():
+#             pass
