72e553735e968eae4b874627f5ba08773150c81d
Nick Roberts <njroberts@dewberry.com>
2023-07-03 12:48:15 -0400

    Do modification of upload params in main mirror.py

--- blobfish/aorc/mirror.py
+++ blobfish/aorc/mirror.py
@@ -8,12 +8,22 @@ import boto3
 from const import FIRST_RECORD, FTP_HOST, RFC_INFO_LIST, RFC_TAR_SHP_URL
 from shapely.geometry import MultiPolygon, Polygon
 from shapely import convex_hull
+from urllib.parse import quote
 
 
 @dataclass
 class RFCFeature:
     name: str
     geom: Polygon | MultiPolygon
+
+
+@dataclass
+class BasicDescriptors:
+    title: str
+    id: str
+    name: str
+    url: str
+    notes: str
 
 
 def get_rfc_features() -> dict[str, RFCFeature]:
@@ -41,13 +51,18 @@ def create_s3_resource(access_key_id: str, secret_access_key: str, region_name: 
 
 
 def create_mirror_dataset_identifiers(
-    start_date: datetime.datetime, end_date: datetime.datetime, rfc_alias: str
-) -> tuple[str, str]:
+    start_date: datetime.datetime, end_date: datetime.datetime, rfc_alias: str, rfc_name: str
+) -> BasicDescriptors:
     dataset_id = f"mirror_{rfc_alias.upper()}_{start_date.strftime('%Y%m')}"
-    dataset_title = (
-        f"Mirror Dataset - {rfc_alias.upper()}, {start_date.strftime('%Y-%m-%d')} to {end_date.strftime('%Y-%m-%d')}"
-    )
-    return dataset_id, dataset_title
+    dataset_name = dataset_id.lower()
+    proper_rfc_name = " ".join([r.capitalize() for r in rfc_name.split()])
+    start_time_formatted = start_date.strftime("%Y-%m-%d")
+    end_time_formatted = end_date.strftime("%Y-%m-%d")
+    dataset_title = f"{proper_rfc_name} Mirror Dataset, {start_time_formatted} to {end_time_formatted}"
+    dataset_description = f"A mirror dataset of AORC precipitation data covering the river forecast center (RFC) region {proper_rfc_name}, from {start_time_formatted} to {end_time_formatted}"
+    dataset_url = quote(dataset_id)
+    descriptors = BasicDescriptors(dataset_title, dataset_id, dataset_name, dataset_url, dataset_description)
+    return descriptors
 
 
 if __name__ == "__main__":
@@ -90,25 +105,34 @@ if __name__ == "__main__":
             nc4_meta.spatial_resolution_meters,
         )
         source_dataset_ttl = source_dataset.serialize(format="ttl")
-        mirror_dataset_id, mirror_dataset_title = create_mirror_dataset_identifiers(
-            nc4_meta.start_time, nc4_meta.end_time, streamed_zip.rfc_alias
+        descriptors = create_mirror_dataset_identifiers(
+            nc4_meta.start_time, nc4_meta.end_time, streamed_zip.rfc_alias, rfc_feature.name
         )
         prov_meta = retrieve_meta()
         upload_mirror_to_ckan(
             os.environ["CKAN_URL"],
             os.environ["CKAN_API_KEY"],
+            descriptors.id,
+            descriptors.name,
             os.environ["CKAN_DATA_GROUP"],
-            mirror_dataset_id,
-            mirror_dataset_title,
-            streamed_zip.last_modified,
-            prov_meta,
-            nc4_meta.start_time,
-            nc4_meta.end_time,
+            descriptors.title,
+            descriptors.url,
+            descriptors.notes,
+            streamed_zip.last_modified.replace(tzinfo=None).isoformat(),
+            prov_meta.remote_docker_file,
+            prov_meta.remote_compose_file,
+            prov_meta.docker_image,
+            prov_meta.git_repo,
+            prov_meta.commit_hash,
+            prov_meta.docker_repo,
+            prov_meta.digest_hash,
+            nc4_meta.start_time.isoformat(),
+            nc4_meta.end_time.isoformat(),
             str(timedelta_to_xsd_duration(nc4_meta.temporal_resolution)),
             nc4_meta.spatial_resolution_meters,
             streamed_zip.rfc_alias,
             rfc_feature.name,
-            rfc_feature.geom,
+            rfc_feature.geom.wkt,
             command_list,
             source_dataset_ttl,
         )
