e1572efa81b60af1d54fcb7ed2e44bc78ae6524f
Nick Roberts <njroberts@dewberry.com>
2023-07-07 11:53:40 -0400

    First draft of transposition metadata creator

---
+++ blobfish/aorc/transposition_utils/ms.py
@@ -0,0 +1,53 @@
+import datetime
+from typing import Any, Iterator
+
+from classes.transposition import TranspositionMetadata
+from meilisearch import Client
+
+
+def create_meilisearch_client(host: str, api_key: str) -> Client:
+    ms_client = Client(host, api_key=api_key)
+    return ms_client
+
+
+def paginate_search_result(ms_client: Client, index: str, query: str) -> Iterator[dict]:
+    offset = 0
+    limit = 10
+    while True:
+        result = ms_client.index(index).search(query, {"offset": offset, "limit": limit})
+        if len(result["hits"]) == 0:
+            break
+        for hit in result["hits"]:
+            yield hit
+        offset += limit
+
+
+def retrieve_ms_data(ms_client: Client, index: str) -> Iterator[TranspositionMetadata]:
+    for hit in paginate_search_result(ms_client, index, ""):
+        start_date_str = hit["start"]["datetime"]
+        duration = hit["duration"]
+        hit_meta = hit["metadata"]
+        hit_stats = hit["stats"]
+        hit_geom = hit["geom"]
+        start_date_dt = datetime.datetime.fromisoformat(start_date_str)
+        end_date_dt = start_date_dt + datetime.timedelta(hours=duration)
+        meta = TranspositionMetadata(
+            hit["id"],
+            duration,
+            start_date_dt,
+            end_date_dt,
+            hit_meta["watershed_name"],
+            hit_meta["watershed_source"],
+            hit_meta["transposition_domain_name"],
+            hit_meta["transposition_domain_source"],
+            hit_meta["png"],
+            hit_stats["count"],
+            hit_stats["mean"],
+            hit_stats["max"],
+            hit_stats["min"],
+            hit_stats["sum"],
+            hit_stats.get("norm_mean"),
+            hit_geom["center_x"],
+            hit_geom["center_y"],
+        )
+        yield meta
---
+++ blobfish/aorc/transposition_utils/rdf.py
@@ -0,0 +1,34 @@
+import datetime
+
+from classes.namespaces import AORC, LOCN
+from general_utils.ckan import query_ckan_catalog
+from rdflib import DCAT, DCTERMS, XSD, Literal, URIRef
+from rdflib.namespace._GEO import GEO
+
+
+def retrieve_composite_dataset_uris(
+    ckan_base_url: str, start: datetime.datetime, end: datetime.datetime
+) -> list[URIRef]:
+    if not ckan_base_url.endswith("/"):
+        ckan_base_url += "/"
+    mirror_catalog_url = ckan_base_url + "aorc_CompositeDataset/catalog.ttl"
+    catalog_graph = query_ckan_catalog(mirror_catalog_url)
+    query_string = """
+        SELECT  ?cda
+        WHERE   {
+            ?cda a aorc:CompositeDataset .
+            ?cda dct:temporal ?t .
+            ?t dcat:startDate ?sd .
+            ?t dcat:endDate ?ed .
+        }
+        FILTER(?start <= ?sd && ?ed <= ?end)
+    """
+    start_date_literal = Literal(start.isoformat(), datatype=XSD.dateTime)
+    end_date_literal = Literal(end.isoformat(), datatype=XSD.dateTime)
+    results = catalog_graph.query(
+        query_string,
+        initNs={"aorc": AORC, "dcat": DCAT, "geo": GEO, "locn": LOCN, "dct": DCTERMS},
+        initBindings={"start": start_date_literal, "end": end_date_literal},
+    )
+    composite_uris = [result_row.cda for result_row in results]
+    return composite_uris
